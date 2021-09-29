974a9d85cb81a5158c7f4c9f2bc3d033d871b4e9
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-09-29 20:34:29 +0200

    Structure some comment.

--- app/src/main/java/com/example/daggertutorial/MainActivity.java
+++ app/src/main/java/com/example/daggertutorial/MainActivity.java
@@ -19,31 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///////////////////// FIELD INJECTION  (TWO METHODS) /////////////////////
+        ///////////////////// FIELD INJECTION /////////////////////
 
-        /// METHOD 1
-        /*  Method 1 :
-        *   With Dagger we don't need to initialize our Car instant object with it attributes, because it Dagger creates all the attributes needed for us.
-        *   To allow Dagger to do so we have to create the Injector interface that is going to initialize our Car instant here.
-        *   And we also have to add the annotation required to Car constructor and it's attributes' constructor.
-        *   Because we added the annotation required and compile the project successfully,
-        *   Dagger created DaggerCarComponent knowing that CarComponent is the Injector interface.
-        *   Then we can use that injector to get our car created by Dagger.
-        * */
-        CarComponent carComponent = DaggerCarComponent.create();
-        car = carComponent.getCar();
-        car.drive();
+            /// METHOD 1
+                //   With Dagger we don't need to initialize our Car instant object with it attributes, because it Dagger creates all the attributes needed for us.
+                //   To allow Dagger to do so we have to create the Injector interface that is going to initialize our Car instant here.
+                //   And we also have to add the annotation required to Car constructor and it's attributes' constructor.
+                //   Because we added the annotation required and compile the project successfully,
+                //   Dagger created DaggerCarComponent knowing that CarComponent is the Injector interface.
+                //   Then we can use that injector to get our car created by Dagger.
+                CarComponent carComponent = DaggerCarComponent.create();
+                car = carComponent.getCar();
+                car.drive();
 
-        /// METHOD 2
-        /*
-        *   A second method to create a car using Dagger is to add the @Inject annotation when declaring the attribute,
-        *   but you should not use private while doing so because Dagger need to access the variable.
-        *   And you have de declare a void method in the Injector interface,
-        *   that methode must have the activity where you're going to create the Car as parameter(MainActivity here).
-        *   Then you can call that method and pass the activity, so that Dagger do the job.
-        * */
-        carComponent.injectInMainActivity(this);
-        car2.drive();
+            /// METHOD 2
+                //
+                //  A second method to create a car using Dagger is to add the @Inject annotation when declaring the attribute,
+                //  but you should not use private while doing so because Dagger need to access the variable.
+                //  And you have de declare a void method in the Injector interface,
+                //  that methode must have the activity where you're going to create the Car as parameter(MainActivity here).
+                //  Then you can call that method and pass the activity, so that Dagger do the job.
+                carComponent.injectInMainActivity(this);
+                car2.drive();
+
+        ///////////////////// METHOD INJECTION /////////////////////
+
 
     }
 }
