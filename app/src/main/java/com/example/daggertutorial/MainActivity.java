de1fd65796b1d4ecb607e4943f133232b38d3691
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-09-29 18:53:01 +0200

    Using the Injector CarComponent to create a car instance.

--- app/src/main/java/com/example/daggertutorial/MainActivity.java
+++ app/src/main/java/com/example/daggertutorial/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.daggertutorial.model.Car;
+import com.example.daggertutorial.model.DaggerInjector.CarComponent;
+import com.example.daggertutorial.model.DaggerInjector.DaggerCarComponent;
 
 public class MainActivity extends AppCompatActivity {
     private Car car;
@@ -16,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /*
-        *   With Dagger we don't need to initialize our Car instant object, because it does it for us.
-        *   Since we are using Dagger we should create the Injector interface that is going to initialize our Car instant here.
+        *   With Dagger we don't need to initialize our Car instant object with it attributes, because it Dagger creates all the attributes needed for us.
+        *   To allow Dagger to do so we have to create the Injector interface that is going to initialize our Car instant here.
+        *   And we also have to add the annotation required to Car constructor and it's attributes' constructor.
+        *   Because we added the annotation required and compile the project successfully, Dagger created DaggerCarComponent knowing that CarComponent is the Injector interface.
+        *   Then we can use that injector to get our car created by Dagger.
         * */
+
+        CarComponent carComponent = DaggerCarComponent.create();
+
+        car = carComponent.getCar();
         car.drive();
 
     }
